978a69622cf6b99b97328c84684a4c195e1f8d53
Crowdbotics <team@crowdbotics.com>
2020-10-05 01:07:40 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -10,6 +10,10 @@ class CustomText(models.Model):
     title = models.CharField(
         max_length=150,
     )
+    location = models.TimeField(
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.title
