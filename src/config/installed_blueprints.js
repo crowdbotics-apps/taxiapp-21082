927d3a8dd56515f7d063fab35256ea6d15ed5f5e
Crowdbotics <team@crowdbotics.com>
2020-10-05 00:59:12 +0000

    add package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'NotificationList126143', human_name: 'Notification List', access_route: 'NotificationList126143'},
 { name: 'Settings126142', human_name: 'Settings', access_route: 'Settings126142'},
 { name: 'Settings126134', human_name: 'Settings', access_route: 'Settings126134'},
 { name: 'UserProfile126132', human_name: 'User Profile', access_route: 'UserProfile126132'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import NotificationList126143Navigator from '../features/NotificationList126143/navigator';
 import Settings126142Navigator from '../features/Settings126142/navigator';
 import Settings126134Navigator from '../features/Settings126134/navigator';
 import UserProfile126132Navigator from '../features/UserProfile126132/navigator';
@@ -17,6 +18,7 @@ import BlankScreen0126110Navigator from '../features/BlankScreen0126110/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+NotificationList126143: { screen: NotificationList126143Navigator },
 Settings126142: { screen: Settings126142Navigator },
 Settings126134: { screen: Settings126134Navigator },
 UserProfile126132: { screen: UserProfile126132Navigator },
